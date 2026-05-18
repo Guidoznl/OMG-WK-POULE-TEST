ee8915daaab790b5176f96efd14c6998d168bf52
Guidoznl <guidoznl1@gmail.com>
2026-05-18 15:37:52 +0200

    Refactor RootLayout to remove ThemeProvider
    Removed ThemeProvider wrapper from RootLayout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import '@/styles/globals.css'
-import { ThemeProvider } from '@/components/ThemeProvider'
 
 export const metadata: Metadata = {
   title: 'OppoSuits WK Poule \'26',
@@ -9,10 +8,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="nl" data-theme="dark">
-      <body>
-        <ThemeProvider>{children}</ThemeProvider>
-      </body>
+    <html lang="nl">
+      <body>{children}</body>
     </html>
   )
 }
